ecea6843d1830dcd8ff26f3118f02dc57ce38370
Mati <matiborlak@gmail.com>
2017-03-02 14:45:42 +0200

    Search filters on collection and movies

--- public/collectionCtrl.js
+++ public/collectionCtrl.js
@@ -2,21 +2,13 @@ app.controller('collectionCtrl', function($scope,service) {
 	//define variable in scope of controller
 	$scope.collection = service.collection;
 
-	$scope.removal = false;
 	$scope.delete = false;
 
 	$scope.iconSwitch = function () {
 		this.delete = !this.delete;
-		this.removal = !this.removal;
 	}
 
-	//define functions in scope of controller
-	/*$scope.addText = function () {
-		$scope.allText.push($scope.textInput);
-		$scope.textInput = "";
+	$scope.remove = function () {
+		this.collection.splice(this.$index,1);
 	}
-
-	$scope.removeText = function() {
-		$scope.allText.splice(this.$index,1);
-	}*/
 })
--- public/moviesCtrl.js
+++ public/moviesCtrl.js
@@ -3,7 +3,9 @@ app.controller('moviesCtrl', function($scope,service) {
 	$scope.movies = service.movies;
 
 	$scope.collect = function () {
+		
 		this.$parent.collection.push(this.movies[this.$index]);
-		console.log(this.$parent.collection);
+
+		console.log(this.$parent.collection.indexOf(this.movies[this.$index]));
 	}
 })
